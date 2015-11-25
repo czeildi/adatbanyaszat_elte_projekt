895d8171723773170626894a6933e25c01c214d5
Ildiko Czeller <iczeller@emarsys.com>
2015-11-25 11:48:23 +0100

    transform binary columns into 1

--- src/load.R
+++ src/load.R
@@ -2,6 +2,11 @@ library(data.table)
 library(rmarkdown)
 library(ggplot2)
 
+# weekday_columns <- names(news)[32:38]
+# data_channel_columns <- names(news)[14:19]
+# kw_columns <- names(news)[20:28]
+# lda_columns <- names(news)[40:44]
+
 getNewsData <- function(filename) {
     fread(filename)
 }
