c8a5a425abed1b01250fc1ea6b59aa6f3a4303e6
Ildiko Czeller <iczeller@emarsys.com>
2015-12-01 16:36:55 +0100

    make basic summary and distribution plots

--- src/load.R
+++ src/load.R
@@ -2,11 +2,14 @@ library(data.table)
 library(rmarkdown)
 library(ggplot2)
 
-# weekday_columns <- names(news)[32:38]
-# data_channel_columns <- names(news)[14:19]
-# kw_columns <- names(news)[20:28]
-# lda_columns <- names(news)[40:44]
-
 getNewsData <- function(filename) {
     fread(filename)
 }
+
+news <- getNewsData(file.path('data', 'OnlineNewsPopularity.csv'))
+
+weekday_columns <- names(news)[32:38]
+data_channel_columns <- names(news)[14:19]
+kw_columns <- names(news)[20:28]
+lda_columns <- names(news)[40:44]
+
--- src/transform.R
+++ src/transform.R
@@ -1,13 +1,12 @@
 get_readable_news <- function(news) {
-    weekday_columns <- names(news)[32:38]
-    data_channel_columns <- names(news)[14:19]
     readable_news <- news
-    readable_news$weekday <- apply(readable_news[,weekday_columns,with = F],1,
-                                   function(x) weekday_columns[which(x==1)])
+    readable_news[,"weekday":= apply(readable_news[,weekday_columns,with = F],1,
+                                   function(x) weekday_columns[which(x==1)])]
     # readable_news$num_channel <- apply(readable_news[,data_channel_columns, with = F],1,
     #                                    function(x) sum(x))
     readable_news$channel <- apply(readable_news[,data_channel_columns, with = F],1,
                                    function(x) data_channel_columns[which(x==1)])
+    readable_news[,"channel" := ifelse(length(channel) == 0, "no_channel", channel[1])]
     
     # num_channel is 0 or 1
     readable_news <- readable_news[,weekday_columns:=NULL, with = F]
