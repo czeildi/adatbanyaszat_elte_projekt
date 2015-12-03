0cbd574b2287919acfc8d472acdaf122622fec2e
Ildiko Czeller <iczeller@emarsys.com>
2015-12-04 00:39:30 +0100

    refactor

--- src/load.R
+++ src/load.R
@@ -21,27 +21,45 @@ data_channel_columns <- names(news)[13:18]
 kw_columns <- names(news)[19:27]
 lda_columns <- names(news)[39:43]
 
-set.seed(123456)
-bound <- floor((nrow(news)/5)*3)
 
-news_for_sampling <- news[sample(nrow(news)), ] 
-train <- news_for_sampling[1:bound,]
-test <- news_for_sampling[(bound+1):nrow(news), ]  
+train_news <- function(news, seed = 123456) {
+    set.seed(123456)
+    bound <- floor((nrow(news)/5)*3)
+    news_for_sampling <- news[sample(nrow(news)), ] 
+    train <- news_for_sampling[1:bound,]
+    train
+}
 
-percent_of_not_popular <- 0.9
-limit_of_popularity <- quantile(news$shares, percent_of_not_popular)
-popular_train <- train
-popular_test <- test
-popular_train$is_popular <- (popular_train$shares > limit_of_popularity)
-popular_train$shares <- NULL
-popular_train$is_popular <- as.factor(popular_train$is_popular)
-popular_test$is_popular <- (popular_test$shares > limit_of_popularity)
-popular_test$shares <- NULL
-popular_test$is_popular <- as.factor(popular_test$is_popular)
+small_data <- function(big_data, size = 1000) {
+    small <- copy(big_data)
+    small[sample(.N, size)]
+}
 
-popular_train_wo_target <- copy(popular_train)
-popular_train_wo_target$is_popular <- NULL
-popular_test_wo_target <- copy(popular_train)
-popular_test_wo_target$is_popular <- NULL
+test_news <- function(news, seed = 123456) {
+    set.seed(123456)
+    bound <- floor((nrow(news)/5)*3)
+    news_for_sampling <- news[sample(nrow(news)), ] 
+    test <- news_for_sampling[(bound+1):nrow(news), ]  
+    test
+}
 
+# train <- train_news(news)
+# small_train <- small_data(train)
+# test <- test_news(news)
+# small_test <- small_data(test)
 
+get_popular_data <- function(news, percent_of_not_popular = 0.9) {
+    limit_of_popularity <- quantile(news$shares, percent_of_not_popular)
+    popular <- copy(news)
+    popular$is_popular <- (popular_test$shares > limit_of_popularity)
+    popular$is_popular <- as.factor(popular$is_popular)
+    popular$shares <- NULL
+    popular
+}
+
+# popular <- get_popular_data(news)
+# popular_train <- train_news(popular)
+# popular_test <- test_news(popular)
+# small_popular_train <- small_data(popular_train)
+# small_popular_test <- small_data(popular_test)
+# 
