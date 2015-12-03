6a2806134f2cc92516adcb439c7301771e4e4ac7
Ildiko Czeller <iczeller@emarsys.com>
2015-12-03 13:24:00 +0100

    models with caret

--- src/load.R
+++ src/load.R
@@ -1,6 +1,11 @@
 library(data.table)
 library(rmarkdown)
 library(ggplot2)
+library(caret)
+library(class)
+library(rpart)
+library(e1071)
+library(neuralnet)
 
 getNewsData <- function(filename) {
     data <- fread(filename)
@@ -15,4 +20,28 @@ weekday_columns <- names(news)[31:37]
 data_channel_columns <- names(news)[13:18]
 kw_columns <- names(news)[19:27]
 lda_columns <- names(news)[39:43]
+
+set.seed(123456)
+bound <- floor((nrow(news)/5)*3)
+
+news_for_sampling <- news[sample(nrow(news)), ] 
+train <- news_for_sampling[1:bound,]
+test <- news_for_sampling[(bound+1):nrow(news), ]  
+
+percent_of_not_popular <- 0.9
+limit_of_popularity <- quantile(news$shares, percent_of_not_popular)
+popular_train <- train
+popular_test <- test
+popular_train$is_popular <- (popular_train$shares > limit_of_popularity)
+popular_train$shares <- NULL
+popular_train$is_popular <- as.factor(popular_train$is_popular)
+popular_test$is_popular <- (popular_test$shares > limit_of_popularity)
+popular_test$shares <- NULL
+popular_test$is_popular <- as.factor(popular_test$is_popular)
+
+popular_train_wo_target <- copy(popular_train)
+popular_train_wo_target$is_popular <- NULL
+popular_test_wo_target <- copy(popular_train)
+popular_test_wo_target$is_popular <- NULL
+
 
--- src/prediction_models.R
+++ src/prediction_models.R
@@ -5,6 +5,9 @@ lm <- lm(shares ~ kw_avg_avg, data = news)
 news$lin_pred1 <- lm$fitted.values
 news$lin_pred1_res <- lm$residuals
 
+# linear model ------------------------------------------------------------
+
+
 ggplot(news, aes(x = kw_avg_avg, y = shares)) + geom_point() +
     geom_smooth(method = "lm", formula = y ~ x)
 
@@ -17,21 +20,85 @@ ggplot(news, aes(x = sqrt(kw_avg_avg), y = sqrt(shares))) + geom_point() +
 
 cor(sqrt(news$shares), sqrt(news$kw_avg_avg))
 
-library('rpart')
-percent_of_not_popular <- 0.9
-limit_of_popularity <- quantile(news$shares, percent_of_not_popular)
-news$is_popular <- (news$shares > limit_of_popularity)
-news$shares <- NULL
-tree <- rpart(is_popular ~ ., data = news)
-printcp(tree)
-plotcp(tree)
 
-tree2 <- rpart(is_popular ~ kw_avg_avg + timedelta + num_keywords, data = news)
-printcp(tree2)
+# knn ---------------------------------------------------------------------
 
-#clustering
 
-# k nearest neighbors
-library('class')
-news$knn_pred <- FALSE
-news[5001:nrow(news)]$knn_pred <- knn1(news[1:5000,], news[5001:nrow(news)], cl = news[1:5000,]$is_popular)
+
+# preProcValues <- preProcess(x = popular_train_wo_target,method = c("center", "scale"))
+ctrl <- trainControl(method="repeatedcv",repeats = 3)
+popular_train <- popular_train[sample(.N, 1000)]
+popular_test <- popular_test[sample(.N, 1000)]
+knnFit <- train(is_popular ~ ., data = popular_train, method = "knn",
+                trControl = ctrl, preProcess = c("center","scale"), tuneLength = 20)
+
+plot(knnFit)
+
+knnPredict <- predict(knnFit,newdata = popular_test)
+#Get the confusion matrix to see accuracy value and other parameter values
+confusionMatrix(knnPredict, popular_test$is_popular)
+
+
+#knn with kw_avg_avg
+ctrl <- trainControl(method="repeatedcv",repeats = 3)
+popular_train <- popular_train[sample(.N, 1000), c("kw_avg_avg", "is_popular"), with = F]
+popular_test <- popular_test[sample(.N, 1000), c("kw_avg_avg", "is_popular"), with = F]
+knnFit <- train(is_popular ~ ., data = popular_train, method = "knn",
+                trControl = ctrl, preProcess = c("center","scale"), tuneLength = 20)
+
+plot(knnFit)
+
+knnPredict <- predict(knnFit,newdata = popular_test)
+#Get the confusion matrix to see accuracy value and other parameter values
+confusionMatrix(knnPredict, popular_test$is_popular)
+
+
+# SVM ---------------------------------------------------------------------
+
+train <- train[sample(.N, 1000)]
+test <- test[sample(.N, 1000)]
+ctrl <- trainControl(method = "repeatedcv", repeats = 10)
+set.seed(1500)
+mod <- train(shares~., data=train, method = "svmLinear", trControl = ctrl)
+svmPredict <- predict(mod, newdata = test)
+
+ctrl <- trainControl(method = "repeatedcv", repeats = 10)
+set.seed(1500)
+mod <- train(shares~., data=train, method = "svmRadial", trControl = ctrl, preProc = c("center", "scale"))
+
+svmPredict <- predict(mod, newdata = test)
+
+ctrl <- trainControl(method = "repeatedcv", repeats = 2)
+set.seed(1500)
+mod <- train(shares~., data=train, method = "svmPoly", trControl = ctrl, preProc = c("center", "scale"))
+
+svmPredict <- predict(mod, newdata = test)
+
+# random forest -----------------------------------------------------------
+
+rf_model<-train(is_popular~.,data=popular_train, method="rf",
+                trControl=trainControl(method="cv",number=5),
+                preProc = c("center", "scale"), prox = TRUE)
+
+rfPred <- predict(rf_model, newdata = popular_test)
+
+# naive bayes -------------------------------------------------------------
+
+set.seed(3456)
+fit3 <- train(is_popular ~ ., 
+              data=popular_train,
+              method = "nb",
+              trControl = trainControl(method="cv", number = 5),
+              tuneGrid = data.frame(fL=0, usekernel=FALSE))
+
+pred3 <- predict(fit3, popular_test, type="raw")
+#Warnings that probability is 0 for some cases
+confusionMatrix(pred3, popular_test$is_popular)
+
+
+# neural network ----------------------------------------------------------
+nnModel <- train(shares~., data = test, method = "neuralnet",
+                 algorithm = 'backprop', learningrate = 0.25,
+                 trControl = trainControl(method = "repeatedcv", repeats = 2), linout = TRUE)
+
+nnModel <- neuralnet(shares ~ kw_avg_avg + timedelta, train, hidden=5, threshold=0.1)
