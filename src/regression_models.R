711a987b5771da07eec06527ea744019bfb59626
Ildiko Czeller <iczeller@emarsys.com>
2015-12-09 02:52:18 +0100

    regression models

--- src/regression_models.R
+++ src/regression_models.R
@@ -7,36 +7,86 @@ library(e1071)
 
 # SVM ---------------------------------------------------------------------
 
-train <- train[sample(.N, 1000)]
-test <- test[sample(.N, 1000)]
-ctrl <- trainControl(method = "repeatedcv", repeats = 10)
-set.seed(1500)
-mod <- train(shares~., data=train, method = "svmLinear", trControl = ctrl)
-svmPredict <- predict(mod, newdata = test)
+train <- train_news(news)
+train <- train[,"shares":= log(shares)]
+small_train <- small_data(train)
+test <- test_news(news)
+test <- test[,"shares":= log(shares)]
+small_test <- small_data(test)
 
-ctrl <- trainControl(method = "repeatedcv", repeats = 10)
-set.seed(1500)
-mod <- train(shares~., data=train, method = "svmRadial", trControl = ctrl, preProc = c("center", "scale"))
+ctrl <- trainControl(method = "repeatedcv", repeats = 3)
+linsvmFit <- train(shares~., data=small_train, method = "svmLinear",
+                   trControl = ctrl, preProc = c("center", "scale"))
+linsvmFit
 
-svmPredict <- predict(mod, newdata = test)
+small_test$linsvm <- predict(linsvmFit, newdata = small_test)
+sqrt(mean((small_test$linsvm - small_test$shares)*(small_test$linsvm - small_test$shares)))
 
-ctrl <- trainControl(method = "repeatedcv", repeats = 2)
-set.seed(1500)
-mod <- train(shares~., data=train, method = "svmPoly", trControl = ctrl, preProc = c("center", "scale"))
 
-svmPredict <- predict(mod, newdata = test)
+train <- train_news(news)
+train <- train[,"shares":= log(shares)]
+small_train <- small_data(train)
+test <- test_news(news)
+test <- test[,"shares":= log(shares)]
+small_test <- small_data(test)
+
+ctrl <- trainControl(method = "repeatedcv", repeats = 3)
+radialsvmFit <- train(shares~., data=small_train, method = "svmRadial",
+                   trControl = ctrl, preProc = c("center", "scale"))
+
+radialsvmFit
+
+small_test$radialsvm <- predict(radialsvmFit, newdata = small_test)
+sqrt(mean((small_test$radialsvm - small_test$shares)*(small_test$radialsvm - small_test$shares)))
+
+
+# train <- train_news(news)
+# train <- train[,"shares":= log(shares)]
+# small_train <- small_data(train)
+# test <- test_news(news)
+# test <- test[,"shares":= log(shares)]
+# small_test <- small_data(test)
+# 
+# ctrl <- trainControl(method = "repeatedcv", repeats = 3)
+# ploysvmFit <- train(shares~., data=small_train, method = "svmPoly",
+#                       trControl = ctrl, preProc = c("center", "scale"))
+# ploysvmPredict <- predict(ploysvmFit, newdata = test)
+# 
+# ploysvmFit
+
 
 # random forest -----------------------------------------------------------
 
-rf_model<-train(is_popular~.,data=popular_train, method="rf",
-                trControl=trainControl(method="cv",number=5),
+train <- train_news(news)
+train <- train[,"shares":= log(shares)]
+small_train <- small_data(train)
+test <- test_news(news)
+test <- test[,"shares":= log(shares)]
+small_test <- small_data(test)
+
+ctrl <- trainControl(method = "repeatedcv", repeats = 3)
+rfFit <- train(shares ~ .,data = small_train, method = "rf",
+                trControl=ctrl,
                 preProc = c("center", "scale"), prox = TRUE)
 
-rfPred <- predict(rf_model, newdata = popular_test)
+rfFit
+
+small_test$rf <- predict(rfFit, newdata = small_test)
+sqrt(mean((small_test$rf - small_test$shares)*(small_test$rf - small_test$shares)))
 
 # neural network ----------------------------------------------------------
-nnModel <- train(shares~., data = test, method = "neuralnet",
-                 algorithm = 'backprop', learningrate = 0.25,
-                 trControl = trainControl(method = "repeatedcv", repeats = 2), linout = TRUE)
+train <- train_news(news)
+train <- train[,"shares":= log(shares)]
+small_train <- small_data(train)
+test <- test_news(news)
+test <- test[,"shares":= log(shares)]
+small_test <- small_data(test)
 
-nnModel <- neuralnet(shares ~ kw_avg_avg + timedelta, train, hidden=5, threshold=0.1)
+nnFit <- train(shares~., data = small_train, method = "nnet",
+               algorithm = 'backprop', learningrate = 0.25, maxit = 50)
+
+nnFit
+
+small_test$nn <- predict(nnFit, newdata = small_test)
+sqrt(mean((small_test$nn - small_test$shares)*(small_test$nn - small_test$shares)))
+
